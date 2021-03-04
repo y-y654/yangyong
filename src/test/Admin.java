6fbce606c44cd9b67d95607c9148f3c36ac2c359
username(y-y654) <872485670@qq.com>
2021-03-04 21:47:49 +0800

    tese and Admin

---
+++ src/test/Admin.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Admin {
+    public static void main(String[] args) {
+        System.out.println("西出阳关无故人");
+    }
+}
---
+++ src/test/Test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我们都是一家人");
+        System.out.println("你好，大家好");
+    }
+}
